4bf3bd388d60d4cea716e05be3a6a6f753feb21d
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-26 22:38:27 -0400

    add corrections to nodes and better comments

--- treesearch.js
+++ treesearch.js
@@ -6,24 +6,29 @@ const breadthfirst = (node, element) => {
   if (node.element) return "found";
   // assume the node being sent in is the root and search from there
   // breadth first search all immediate children of the given node
-  // take the children and add them to search list
-  // 
-  searchlist = children;
-  //
+  // siblings and children are arrays
+  // push all the elements in the siblings array into the searching array
+  // visit the siblings by passing the sibling node into the function but before that pop it out of the array of siblings
+
+
 };
+alice = {
+  "children":["sameer", "vince"],
+  "element": false,
+  "parent": "treeroot"
+}
 // example child of treeroot
-treenode = {
+Johnathan = {
   "children":["alice", "billy", "gregory", "arnold", "melanie"],
-  "element": "false",
+  "element": false,
   "parent": "treeroot"
 }
 // root of a tree
 treeroot = {
-  "children": ["Johnothan", "Khayo", "simple questions", "Llort"],
+  "children": ["Johnathan", "Khayo", "simple questions", "Llort"],
   "element": true,
   "parent": null
 }
-
 
 
 
