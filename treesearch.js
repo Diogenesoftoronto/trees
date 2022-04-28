abf70bd181aee3294bf47f25c4d8641f83fa6512
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-27 23:05:57 -0400

    add better comments and fix

--- treesearch.js
+++ treesearch.js
@@ -3,37 +3,51 @@ const breadthfirst = (node) => {
   parent = node.parent;
   siblings = parent.children;
   // base case is that the element is present on the node
-  if (node.element) return "found";
+  if (node.element) return "found"
+  // initialize searching as an array if it does not exist
+  
+  if (searching[0] === "undefined"){const searching = []; searching.concat(siblings)}
   // assume the node being sent in is the root and search from there
   // breadth first search all immediate children of the given node
   // siblings and children are arrays
-  // push all the elements in the siblings array into the searching array
-  searching.concat(siblings)
-  // visit the siblings by passing the sibling node into the function but before that pop it out of the array of siblings
+  // concat all the elements in the siblings array into the searching array
+  // visit the siblings by passing the sibling node into the 
+  // function but before that pop it out of the array of siblings
   siblingBeingSearched = searching.pop()
-  return breadthfirst(siblingBeingSearched)
+  // if there are no siblings left to be searched then the function ret
+  if (siblingBeingSearched) return breadthfirst(siblingBeingSearched)
+  return breadthfirst()
+}; // 
 
-};
+billy = {
+  "children":[],
+  "element": true,
+  "parent": "Johnathan"
+}
 alice = {
-  "children":["sameer", "vince"],
-  "element": false,
-  "parent": "treeroot"
+  "children":[],
+  "element": true,
+  "parent": "Johnathan"
 }
 // example child of treeroot
 Johnathan = {
-  "children":["alice", "billy", "gregory", "arnold", "melanie"],
+  "children": [alice, billy],
   "element": false,
-  "parent": "treeroot"
+  "parent": treeroot
 }
 
 // root of a tree
 treeroot = {
-  "children": ["Johnathan", "Khayo", "simple questions", "Llort"],
+  "children": [Johnathan],
+  "element": false,
+  "parent": null
+}
+
+baseroot = {
+  "children": [],
   "element": true,
   "parent": null
 }
 
 
-
-
-console.log(searchlistbreadthfirst(treeroot)); // should return found
+console.log(searchlistbreadthfirst(baseroot)); // should return found
